c7fbeb280cc66ac27043e76e48094438da0eb593
karen <kkwentus@chef.io>
2018-04-14 15:09:57 -0500

    v2

--- issue.js
+++ issue.js
@@ -1,10 +1,36 @@
 /** Opens a Github issue with data provided by the webhook POST request  */
+const octokit = require ('@octokit/rest')()
 
+octokit.authenticate({
+    type: 'token',
+    token: '837b7d8063aabf2dcd9935b9efdea009fb972af9'
+})
+
+//async function open(repoName, orgName, userName){
 var open = (repoName, orgName, userName) => {
+      //debugging
       console.log ('reponame is', repoName);
       console.log ('orgName is', orgName);
       console.log ('userName is', userName);
-};
+
+      const newIssue = octokit.issues.create({
+             owner: 'kkwentus',
+             repo: 'notify-on-delete',
+             title: `${repoName} has been deleted`,
+             body: `This is an auto-generated ticket to track the deletion of repository ${repoName} in the organziation ${orgName} by user ${userName}.  @kkwentus Please review`,
+             assignee: 'kkwentus',
+             labels: 'admin review'
+      },
+        (error, results) => {
+          if(error){
+              console.log('Error on issue creation ===> ', error)
+          }
+          else{
+              console.log('Issue creation results ===>', results)
+          }
+      }); //end newIssue
+
+}; //end open
 
 
 module.exports = {
--- server.js
+++ server.js
@@ -1,29 +1,37 @@
 const express = require('express')
-var bodyParser = require('body-parser')
+const bodyParser = require('body-parser')
 const issue = require('./issue.js')
 
 var app = express();
+var jsonParser = bodyParser.json();
+
+var issueStatus
 
 app.use(bodyParser.json({ type: 'application/*+json' }));
-
-var jsonParser = bodyParser.json();
+//app.use(function(err, req, res, next));
 
 app.get('/', (request, response) => {
     response.send('<h2>notify on delete webserver</h2>');
-    
 });//end get
+
+var evalRequest = (request) => {
+    issueStatus = request.body.action;
+    console.log('Notified for:', issueStatus);
+
+    //we are only taking action if the notification we receive is for a deleted action
+    if (request.body.action === "deleted"){
+        issue.open(request.body.repository.full_name, request.body.organization.login, request.body.sender.login);
+    }
+
+};//end evalRequest
+
 
 app.post('/payload', jsonParser, (request, response) => {
     //console.log('REQUEST DATA FORMAT', typeof request);
     //console.log(request.body);
-    if(request.body.action === "deleted"){
-       issue.open(request.body.repository.full_name, request.body.organization.login, request.body.sender.login);
-    }
-    else {
-        console.log('action was not a delete-->', request.body.action)
-    }
-    response.send('payload received');
-
+    response.send('Payload received');
+    var eval = evalRequest(request); 
+    
 });//end post
 
 //common localhost dev port, for testing will need to forward with ngrok for public URL
